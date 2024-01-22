222b30d1980cec1a98937e35bc967b8649b59863
Tejas Mhadgut <tejasmhadgut@gmail.com>
2024-01-22 16:53:25 -0500

    installed React and Express and wrote backend code for the app

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express()
+const server = require("http").createServer(app);
+const cors = require("cors");
+
+const io = require("socket.io")(server, {
+cors: {
+    origin:"*",
+    methods:["GET","POST"]
+}
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5500;
+
+app.get("/",(req,res)=>{
+    res.send("Server is running..")
+});
+
+io.on('connection', (socket) => {
+    socket.emit("me", socket.id);
+
+    socket.on("disconnect", ()=>{
+        socket.broadcast.emit("callended");
+    });
+
+    socket.on("calluser", ({userToCall, signalData, from, name})=>{
+        io.to(userToCall).emit("calluser", {signal: signalData, from, name});
+    });
+
+    socket.on("answerCall", (data)=>{
+        io.to(data.to).emit("callaccepted", data.signal);
+    });
+});
+
+server.listen(PORT, ()=> console.log(`Server Listening on Port ${PORT}`));
+
